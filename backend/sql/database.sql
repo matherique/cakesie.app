1edc19f4b3d2dfa94e82b485b845b4b8eab3fa09
Matheus Henrique <matherique@gmail.com>
2022-07-16 21:45:32 -0300

    fix: sql file

--- backend/sql/database.sql
+++ backend/sql/database.sql
@@ -9,19 +9,19 @@ CREATE TABLE IF NOT EXISTS cake(
   Name varchar(100) not null,
   Price decimal(10,2) not null,
   Status boolean not null,
-  Description text not null,
+  Description text not null
 );
 
 CREATE TABLE IF NOT EXISTS ingredients(
   Id serial not null primary key,
   ProductId int not null references product(Id),
   CakeId int not null references cake(Id),
-  Quantity int not null,
+  Quantity int not null
 );
 
 CREATE TABLE IF NOT EXISTS stock(
   Id serial not null primary key,
   ProductId int not null references product(Id),
   Quantity int not null,
-  AddedAt timestamp not null default now(),
-)
+  AddedAt timestamp not null default now()
+);
