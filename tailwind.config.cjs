15d09e4d24d368164c4d35d441ff8ee3a4b0ee13
Matheus Henrique <matherique@gmail.com>
2022-08-18 19:27:24 -0300

    feat: chage to use css grid

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        layout: "50px 100px 700px 100px",
+      },
+    },
   },
   plugins: [],
 };
